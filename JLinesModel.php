a1a5377685211249ad9b122b1e81662906804638
Juan David Rodriguez Ramirez <juan.rodriguez@tramasoft.com>
2013-01-18 20:37:26 -0500

    Modelo para el modo factura
    modelo el cual sus propiedades se crean dinamicamente segun el modelo
    que se este usando, para correcta validacion al momento de el uso del
    tipo factura

---
+++ JLinesModel.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Modelo con propiedades dinamicas para usar en las lineas 
+ * cuando es modo editInline = false
+ * 
+ * @uses CFormModel
+ * @author Juan David Rodriguez <juan.rodriguez@tramasoft.com>
+ * @copyright 2013 - Juan David Rodriguez
+ * @license New BSD License 
+ */
+class JLinesModel extends CFormModel{
+    /**
+     * Propiedad para almacenar los attributos de el modelo
+     * @access private
+     * @var array $attributes
+     */
+    private $attributes = array();
+    /** 
+     * Nombre del Modelo del que se esta obteniendo atributos
+     * @var string 
+     */
+    public $modelBase;
+    /**
+     * Reglas de Validacion del Modelo Base
+     * @var array $rules
+     */
+    public $rules = array();
+    /**
+     * Labels que esten definidos en el modelo base
+     * @var array $rules
+     */
+    public $attributeLabels = array();
+    /**
+     * Reglas de Validacion del Modelo Base
+     * @return array 
+     */
+    public function rules(){
+        return $this->rules;
+    }
+    /**
+     * Labels que esten definidos en el modelo base
+     * @return array 
+     */
+    public function attributeLabels(){
+        return $this->attributeLabels;
+    }
+	
+    /**
+     * Asignamos las nuevas propiedades
+     * 
+     * @param string $name
+     * @param string $value 
+     */
+    public function __set($name, $value) {
+        $this->attributes[$name] = $value;
+    }
+    /**
+     * Metodo para retornar el valor de una propiedad
+     * @param string $name
+     * @return string 
+     */
+    public function __get($name) {
+        if(array_key_exists($name, $this->attributes)) {
+            return $this->attributes[$name];
+        }/*else{
+            throw new CException(Yii::t('yii','Property "{class}.{property}" is not defined.',
+			array('{class}'=>get_class($this), '{property}'=>$name)));
+        }*/
+    }
+    /**
+     * Verifica que exista el atributo
+     * 
+     * @param string $name
+     * @return boolean 
+     */
+    public function __isset($name) {
+        return isset($this->attributes[$name]);
+    }
+    /**
+     * Elimina el atributo de la clase
+     * @param string $name 
+     */
+    public function __unset($name) {
+        unset($this->attributes[$name]);
+    }
+    /**
+     *
+     * @param string $name
+     * @param array $arguments
+     * @return mixed el retorno del metodo a llamar 
+     */
+    public function __call($name, $arguments) {
+        echo "Calling object method '$name' "
+             . implode(', ', $arguments). "<br>";
+        if(class_exists($this->modelBase, false) &&  $this->$name instanceof $this->modelBase)
+			return call_user_func_array($this->$name, $parameters);
+        
+        throw new CException(Yii::t('yii','{class} and its behaviors do not have a method or closure named "{name}".',
+			array('{class}'=>get_class($this), '{name}'=>$name)));
+    }
+
+}
+
+?>
